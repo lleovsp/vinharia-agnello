24210577aad136ecbf7d414c1ed59776a758cb57
Miguel <migueldsa2007@gmail.com>
2025-04-15 19:27:50 -0300

    solicitando as informações ao usuário utilizando o prompt

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+nomeVinho = ("Digite o nome do vinho:");
+tipoVinho = ("Digite o tipo do vinho (Tinto, Branco ou Rosé):");
+safraVinho = ("Digite o ano da safra do vinho:");
+
+
+// Adm
+quantidadeEstoque = ("Digite a quantidade em estoque do vinho:");
