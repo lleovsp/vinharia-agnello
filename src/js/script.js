14e100a59081293dbe26fcb43a13b292a3d922ea
João Gabriel C. M. Santos <jgmonticeli00@gmail.com>
2025-04-16 21:01:50 -0300

    Verificação da Quantidade do Estoque

--- src/js/script.js
+++ src/js/script.js
@@ -44,16 +44,28 @@ class MobileNavbar {
   mobileNavbar.init();
 
 var nomeVinho = prompt("Digite o nome do vinho:");
-console.log("Nome do vinho: " + nomeVinho);
 alert("Nome cadastrado com sucesso! Veja os detalhes no console");
+console.log("Nome do vinho: " + nomeVinho);
+
 var tipoVinho = prompt("Digite o tipo do vinho (Tinto, Branco ou Rosé):");
-console.log("Tipo do vinho: " + tipoVinho);
 alert("Tipo do vinho cadastrado com sucesso! Veja os detalhes no console");
+console.log("Tipo do vinho: " + tipoVinho);
+
 var safraVinho = prompt("Digite o ano da safra do vinho:");
-console.log("Ano da Safra: " + safraVinho);
 alert("Ano da Safra do vinho cadastrado com sucesso! Veja os detalhes no console");
+console.log("Ano da Safra: " + safraVinho);
+
 var quantidadeEstoque = prompt("Digite a quantidade em estoque do vinho:");
-console.log("Quantidade em estoque: " + quantidadeEstoque);
-alert("Quantidade em estoque cadastrada com sucesso! Veja os detalhes no console.");
+if (quantidadeEstoque < 5){
+  alert("Verificar o Console para classificação da quantidade de estoque");
+  console.log("Quantidade de estoque: " + quantidadeEstoque);
+  console.log("ESTOQUE BAIXO");
+  alert("Quantidade cadastrada com sucesso!")
+}
+else{
+  alert("Quantidade em estoque cadastrada com sucesso! Veja os detalhes no console.");
+  console.log("Quantidade em estoque: " + quantidadeEstoque);
+}
+
 
 
